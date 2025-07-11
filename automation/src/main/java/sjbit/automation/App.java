10ce170e45e4a17476a69ccdcc0de30dba2b5a04
nikesh-shakti <nikeshshakti@gmail.com>
2025-07-11 15:42:24 +0530

    springboot updated

---
+++ automation/src/main/java/sjbit/automation/App.java
@@ -0,0 +1,30 @@
+package sjbit.automation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       WebDriverManager.chromedriver().setup();
+       WebDriver driver =new ChromeDriver();
+       driver.get("https://www.saucedemo.com");
+       WebElement ele =driver.findElement(By.id("user-name"));
+       WebElement ele1 =driver.findElement(By.id("password"));
+       ele.sendKeys("standard_user");
+       ele1.sendKeys("secret_sauce");
+       WebElement btn=driver.findElement(By.name("login-button"));
+       btn.submit();
+       //river.findElement(By.name("q")).sendKeys("A B De Villiers ");
+    }
+}
